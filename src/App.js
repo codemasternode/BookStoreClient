e98e975e327f2b9c68f325a5eaa59d4a7f65bb75
javaxd123 <marcinwarzybok@outlook.com>
2018-02-17 15:59:20 +0100

    second commit

--- src/App.js
+++ src/App.js
@@ -21,10 +21,23 @@ class App extends React.Component {
 
   render() {
     return (
-      <div>
+      <div className="container">
         <ul>
           {this.state.array.map(item =>
-          <li key={item.id}>{item.name}</li>)}
+            <li key={item.id} className="row">
+              <div className="col-md-3 col-lg-3 col-sm-12 img-wrapper">
+                <img src={item.pictureLink} />
+              </div>
+              <div className="col-md-9 col-lg-9 col-sm-12 item-wrapper">
+                <div className="top-wrapper">
+                  <h1>{item.name}</h1>
+                  <h2>{item.author.name} {item.author.surname}</h2>
+                </div>
+                <div className="text-wrapper">
+                  <p>{item.description}</p>
+                </div>
+              </div>
+            </li>)}
         </ul>
       </div>
     )
