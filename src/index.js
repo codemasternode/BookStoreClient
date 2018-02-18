3a01f4b819975d317f681958ee16770825365a55
javaxd123 <marcinwarzybok@outlook.com>
2018-02-18 22:07:57 +0100

    ready for router

--- src/index.js
+++ src/index.js
@@ -2,7 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+import { BrowserRouter, Route } from 'react-router-dom';
+
 ReactDOM.render(
-    <App />,
-    document.getElementById('root')
+    <BrowserRouter>
+        <Route path="/" component={ App }/>
+    </BrowserRouter>, document.getElementById('root')
 );
