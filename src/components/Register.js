5cabd9d70e7eeba128067fda3faf27ce836dd80c
javaxd123 <marcinwarzybok@outlook.com>
2018-02-22 19:07:45 +0100

    after adding redirecting to home

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Redirect } from 'react-router'
 import '../style/Register.css';
 
 
@@ -11,6 +12,7 @@ class Register extends React.Component {
             username: '',
             email: '',
             password: '',
+            fireRedirect: false
         };
     }
 
@@ -21,6 +23,7 @@ class Register extends React.Component {
         const email = this.state.email;
 
         event.preventDefault();
+        this.setState({ fireRedirect: true });
 
         fetch(`http://localhost:8080/users/register`, {
             headers: {
@@ -33,26 +36,44 @@ class Register extends React.Component {
                 email: email,
                 password: password
             }),
-        });
+        })
+            .then(res => {
+                console.log(res);
+            });
     }
 
-    
+
     render() {
+        const { from } = this.props.location.state || '/'
+        const { fireRedirect } = this.state
         return (
             <div className="container">
-                <form onSubmit={this.handleSubmit}>
-                    <input type="text" onChange={event => this.setState({
-                        username: event.target.value
-                    })}/>
-                    <input type="email" onChange={event => this.setState({
-                        email: event.target.value
-                    })}/>
-                    <input type="password" onChange={event => this.setState({
-                        password: event.target.value
-                    })}/>
-                    <input type="submit" value="Submit" />
+                <form onSubmit={this.handleSubmit} className="register-form">
+                    <h2>Form Register</h2>
+                    <div className="form-group">
+                        <label for="username">Username</label>
+                        <input type="text" id="username" class="form-control" placeholder="Enter Username" onChange={event => this.setState({
+                            username: event.target.value
+                        })} />
+                    </div>
+                    <div className="form-group">
+                        <label for="email">E-mail</label>
+                        <input type="email" id="email" class="form-control" placeholder="Enter email" onChange={event => this.setState({
+                            email: event.target.value
+                        })} />
+                    </div>
+                    <div className="form-group">
+                        <label for="pass">Password</label>
+                        <input class="form-control" id="pass" placeholder="Enter valid password" type="password" onChange={event => this.setState({
+                            password: event.target.value
+                        })} />
+                    </div>
+                    <input type="submit" class="btn btn-lg button-custom-submit" value="Submit" />
+
                 </form>
-                
+                {fireRedirect && (
+                    <Redirect to={from || '/'} />
+                )}
             </div>
         )
     }
